9f4c60cba11be4f7bca0711e0998185dbf533c1b
akorkoshko <66247438+akorkoshko@users.noreply.github.com>
2021-06-15 13:25:09 +0300

    test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ActionController::API
   def index
     @article =  Article.find(1)
 
+
     if stale?(last_modified: @article.updated_at)
       render json: @article
     end
